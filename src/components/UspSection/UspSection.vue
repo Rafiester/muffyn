9ba3145aae19135be990c5689226263ec1a5c9f6
Rafiester <rafi.haqqi@gmail.com>
2026-06-29 21:10:32 +0700

    style: remove arrow icons from services/expertise cards

--- src/components/UspSection/UspSection.vue
+++ src/components/UspSection/UspSection.vue
@@ -71,11 +71,6 @@
               </span>
             </div>
           </div>
-          <div class="service-arrow">
-            <svg viewBox="0 0 24 24" width="24" height="24">
-              <path d="M5 12h14M12 5l7 7-7 7" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" fill="none"/>
-            </svg>
-          </div>
         </div>
 
       </div>
